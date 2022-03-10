5d67115706811eb9631167c162b731cf56fcdb73
vlad10100 <vbucaralde@gmail.com>
2022-03-11 07:02:10 +0800

    signup

--- withUserAuth/authentication/urls.py
+++ withUserAuth/authentication/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from authentication.views import MyTokenObtainPairView
+from authentication.views import MyTokenObtainPairView, UserCreationView
 
 from rest_framework_simplejwt.views import TokenRefreshView
 
 
 urlpatterns = [
     path('api/token/', MyTokenObtainPairView.as_view()),
-    path('api/token/refresh/', TokenRefreshView.as_view())
+    path('api/token/refresh/', TokenRefreshView.as_view()),
+    path('api/signup/', UserCreationView.as_view())
 
 ]
--- withUserAuth/authentication/views.py
+++ withUserAuth/authentication/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import status, serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -6,6 +6,9 @@ from rest_framework.views import APIView
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+from authentication.models import User
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -20,3 +23,29 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+
+
+class UserCreationSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(min_length=2)
+    first_name = serializers.CharField(max_length=100)
+    last_name = serializers.CharField(max_length=100)
+    password = serializers.CharField(min_length=2)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'password']
+    
+    # def validate(self, attrs):
+
+
+class UserCreationView(APIView):
+    serializer_class = UserCreationSerializer
+
+    def post(self, request):
+        data = request.data 
+        serializer = self.serializer_class(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
