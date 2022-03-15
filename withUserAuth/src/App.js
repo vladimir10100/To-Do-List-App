5ce985b65c24384391024290d8359c83a87efa16
vlad10100 <vbucaralde@gmail.com>
2022-03-15 15:15:35 +0800

    with user auth

---
+++ withUserAuth/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Header from './Pages/page_components/Header'
+import HomePage from './Pages/HomePage'
+import LoginPage from './Pages/LoginPage'
+
+
+const App = () => {
+    return (
+        <>
+            <Header/>
+            <HomePage/>
+            <LoginPage />
+        </>
+    )
+}
+export default App
---
+++ withUserAuth/src/Pages/LoginPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const LoginPage = () => {
+    return (
+        <>LoginPage</>
+    )
+}
+
+export default LoginPage
